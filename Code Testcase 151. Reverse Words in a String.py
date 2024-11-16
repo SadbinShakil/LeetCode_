329568e9228c7381cbe17162180f1682b44dccaf
Sakil Sarker <61023087+SadbinShakil@users.noreply.github.com>
2024-11-16 00:39:49 -0500

    Create  Code Testcase 151. Reverse Words in a String.py

---
+++ Code Testcase 151. Reverse Words in a String.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def reverseWords(self, s):
+      temp1 = []
+      a = []
+      for i in range(len(s)):
+        if s[i] != " ":
+          a.append(s[i])
+        elif s[i] == " ":
+          temp1.append("".join(a))
+          a = []
+      temp1.append("".join(a))
+      temp1.reverse()
+      temp1 = " ".join(temp1)
+      temp1 = temp1.split()
+
+      temp1 = ", ".join(temp1)
+      return [temp1]
+
+s = "  Bob    Loves    Alice  "
+solution = Solution()
+solution.reverseWords(s)
